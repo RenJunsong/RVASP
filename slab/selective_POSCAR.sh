1575499727d3e82b1fc95e17a2ffd3e6857125ca
RenJunsong <famugoing@qq.com>
2021-01-06 19:49:14 +0800

    change the slab files

--- slab/selective_POSCAR.sh
+++ slab/selective_POSCAR.sh
@@ -2,22 +2,19 @@
 ## Function: The value greater or less than a certain z-axis is selected to relax.By Ren JS
 ## Usage, ./thisfile.sh or ./thisfile.sh > 0.01
 ## Setting ##
-check=">"     ##  > or <
-value=0.01    ##  `>0.01` is means if the z of atom >0.01, the atom will be selected to relax(add T T T),nor add F F F
+value='>0.24'    ##  `>0.01` is means if the z of atom >0.01, the atom will be selected to relax(add T T T),nor add F F F
 file_name=POSCAR-0
-out_file_name=POSCAR-m
+dos2unix $file_name
+out_file_name=POSCAR
 ## Done ##
-if [ $# -eq 2 ]; then
-check=$1
-value=$2
 head -n 7 $file_name > $out_file_name
 echo -e "Selective\nDirect" >> $out_file_name
 ## the number of last line at POSCAR
-for i in {9..272}
+for i in {9..168}
 do
 low=`sed -n ${i}p $file_name`
 z=`sed -n ${i}p $file_name | awk '{print $3}'`
-check_value=`echo "${z}${check}${value}" | bc` 
+check_value=`echo "${z}${value}" | bc` 
 if [ $check_value -eq 1 ]; then
 echo $low T T T >> $out_file_name
 else
